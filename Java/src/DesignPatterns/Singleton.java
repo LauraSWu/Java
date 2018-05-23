61f8bc136c98b1ab43e1295ebf89c777c0665dc5
laurawu <laurawu@192.168.12.109>
2018-05-23 17:51:21 -0400

    Singleton design pattern

--- Java/src/DesignPatterns/Singleton.java
+++ Java/src/DesignPatterns/Singleton.java
@@ -3,17 +3,39 @@ package DesignPatterns;
 public class Singleton {
 	
 	private static Singleton theOnlyInstance = null;
-	
+
 	//add other public class members here
+	public String s;
 	
+	//private constructor for the class
 	private Singleton() {
-		//private constructor for the class
+		s = "Singleton string";
 	}
 	
+	//returns theOnlyInstance using "lazy initialization"
 	public static Singleton getInstance() {
 		if (theOnlyInstance == null) {
 			theOnlyInstance = new Singleton();
 		}
 		return theOnlyInstance;
 	}
+	
+	/* for multi-threaded programs, use can use either:
+	 1. Synchronized keyword in getInstance method:
+	 
+		public static synchronized Singleton getInstance() {
+
+	 2. Optional helper - anonymous inner class
+	 	Note: This always creates a singleton class, does not use "lazy initialization"
+
+		 public static class SingletonHelper(){
+		 	private static final Singleton innerInstance = new Singleton();
+		 }
+		 
+		 public static Singleton getInstance(){
+		 	return singletonHelper.innerInstance;
+		 }
+
+	 */
+	
 }
